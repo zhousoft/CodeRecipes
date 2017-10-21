da66c839d4f93b580bdd0fd49f3125f420c59ac3
zhouzhongtao <zhouzhongtaosoft@163.com>
2017-10-21 23:37:42 +0800

    更新

--- lua/table_is_empty.lua
+++ lua/table_is_empty.lua
@@ -2,8 +2,10 @@
     判断table是否为空
     不能使用 if t == {} 来判断，实际上比较的是t和一个
     匿名table，结果永远为false
-    #t 判断table长度只能统计table中默认key为数字的元素个数（相当于list）
+    用#t的方法来判断table长度只能统计table中默认key为数字的元素个数（相当于list）
     如 t = {"a","c"="b","d"}, #t = 2
+    t = {"a","b",2="c"}, #t = 2
+    不要在table中用nil来删除元素，会导致#t出现奇怪问题
 --]] 
 
 function table_is_empty(t)
